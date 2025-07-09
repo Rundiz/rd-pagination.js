c4333a7ce237d0461e8b27a7cffdcb8f3d3fcd3e
ve3 <kawewong@gmail.com>
2025-07-09 08:41:07 +0700

    Update JS doc block.

--- assets/js/rd-pagination.js
+++ assets/js/rd-pagination.js
@@ -64,68 +64,71 @@ class RdPagination {
      *              Example 2: `http://domain.tld/my-category?page=%PAGENUMBER%` This URL use page number as query string.<br>
      *              Example 3: `http://domain.tld/my-category?filter=some_filter_values&amp;search=foobar&amp;page=%PAGENUMBER%` This URL use page number as query string with other query strings in it, seperate each query string with `&amp;` not just `&`.<br>
      *              Example 4: `http://domain.tld/my-category?start=%PAGENUMBER%` This URL use page number as query string but use start as the name.<br>
-     *              You have to get the page number value and set its value to this class by call the "page_number_value" option.
+     *              You have to get the page number value and set its value to this class in the option "page_number_value".
      * @param {number} options.page_number_value The current page number value (Required). This class cannot detect current page number automatically because of dynamic styles of URL. So, you have to manually set its value to this option.
-     * @param {number} options.total_records The total number of records (Required). This means "all" records by conditions with out the "LIMIT" or slices commands.
+     * @param {number} options.total_records The total number of records (Required). This means "all" records (maybe with sql `WHERE` conditions but..) with out the "LIMIT" or slices commands.
      * 
      * @param {number} options.items_per_page The number of items that will be displaying per page. Such as number of articles to display in each page. Default is 10.
-     * @param {string} options.page_number_type The page number type. The value can be `start_num` or `page_num`. Default is `start_num`.  
-     *              Start number or `start_num` also know as offset number. (eg. page number value will be 0, 10, 20, 30, ...)  
-     *              Page number or `page_num`. (eg. page number value will be 1, 2, 3, 4, ...)
+     * @param {string} options.page_number_type The page number type. The value can be `start_num` or `page_num`.<br>  
+     *              Start number or `start_num` also know as offset number. (eg. page number value will be 0, 10, 20, 30, ...)<br>
+     *              Page number or `page_num`. (eg. page number value will be 1, 2, 3, 4, ...)<br>
+     *              Default is `start_num`.
      * 
-     * @param {boolean} options.current_page_link Display current link at current page. Set to true to display, false not to display. Default is `false`.
+     * @param {boolean} options.current_page_link Display current link at current page. Set to `true` to display, `false` not to display. Default is `false`.
      * @param {object} options.current_page_link_attributes The current page link attributes in object where key is attribute name. 
      *              Example `{'class' => 'my class'}`. Must not contains `href`, `data-rd-pagination` attributes.
-     * @param {string} options.current_tag_close The current page tag close. If you set to display current page, this will be placed after link to the current page. Default is empty.
-     * @param {string} options.current_tag_open The current page tag open. If you set to display current page, this will be placed before link to the current page. Default is empty.
+     * @param {string} options.current_tag_close The current page close tag. If you set to display current page, this will be placed after link to the current page. Default is empty.
+     * @param {string} options.current_tag_open The current page open tag. If you set to display current page, this will be placed before link to the current page. Default is empty.
      * 
-     * @param {boolean} options.first_page_always_show If you are at first page the first page link will not show if you set this value to false, if you set to true it will be always show the first page link. Default is `false`.
+     * @param {boolean} options.first_page_always_show If you are at first page the first page link will not show if you set this value to `false`, if you set to `true` it will be always show the first page link. Default is `false`.
      * @param {object} options.first_page_link_attributes The first page link attributes in object where key is attribute name. 
      *              Example `{'class' => 'my class'}`. Must not contains `href`, `data-rd-pagination` attributes.
-     * @param {string|false} options.first_page_text The link text of the paginate that will go to the first page. Set to false to not displaying first page link.
-     * @param {string} options.first_tag_close The first page tag close. If you set to display first page, this will be placed after link to the first page. Default is 1 space.
-     * @param {string} options.first_tag_open The first page tag open. If you set to display first page, this will be placed before link to the first page. Default is 1 space.
+     * @param {string|false} options.first_page_text The link text of the paginate that will go to the first page. Set to `false` to not displaying first page link at all.
+     * @param {string} options.first_tag_close The first page close tag. If you set to display first page, this will be placed after link to the first page. Default is 1 space.
+     * @param {string} options.first_tag_open The first page open tag. If you set to display first page, this will be placed before link to the first page. Default is 1 space.
      * 
-     * @param {boolean} options.last_page_always_show If you are at last page the last page link will not show if you set this value to false, if you set to true it will be always show the last page link. Default is `false`.
+     * @param {boolean} options.last_page_always_show If you are at last page the last page link will not show if you set this value to `false`, if you set to `true` it will be always show the last page link. Default is `false`.
      * @param {object} options.last_page_link_attributes The last page link attributes in object where key is attribute name. 
      *              Example `{'class' => 'my class'}`. Must not contains `href`, `data-rd-pagination` attributes.
-     * @param {string|false} options.last_page_text The link text of the paginate that will go to the last page. Set to false to not displaying last page link.
-     * @param {string} options.last_tag_close The last page tag close. If you set to display last page, this will be placed after link to the last page. Defauls is 1 space.
-     * @param {string} options.last_tag_open The last page tag open. If you set to display last page, this will be placed before link to the last page. Default is 1 space.
+     * @param {string|false} options.last_page_text The link text of the paginate that will go to the last page. Set to `false` to not displaying last page link at all.
+     * @param {string} options.last_tag_close The last page close tag. If you set to display last page, this will be placed after link to the last page. Defauls is 1 space.
+     * @param {string} options.last_tag_open The last page open tag. If you set to display last page, this will be placed before link to the last page. Default is 1 space.
      * 
-     * @param {boolean} options.next_page_always_show If you are at last page the next page link will not show if you set this value to false, if you set to true it will be always show the next page link. Default is `false`.
+     * @param {boolean} options.next_page_always_show If you are at last page the next page link will not show if you set this value to `false`, if you set to `true` it will be always show the next page link. Default is `false`.
      * @param {object} options.next_page_link_attributes The next page link attributes in object where key is attribute name. 
      *              Example `{'class' => 'my class'}`. Must not contains `href`, `data-rd-pagination` attributes.
-     * @param {string|false} options.next_page_text The link text of the paginate that will go to the next page. Set to false to not displaying next page link.
-     * @param {string} options.next_tag_close The next page tag close. If you set to display next page, this will be placed after link to the next page. Default is 1 space.
-     * @param {string} options.next_tag_open The next page tag open. If you set to display next page, this will be placed before link to the next page. Default is 1 space.
+     * @param {string|false} options.next_page_text The link text of the paginate that will go to the next page. Set to `false` to not displaying next page link at all.
+     * @param {string} options.next_tag_close The next page close tag. If you set to display next page, this will be placed after link to the next page. Default is 1 space.
+     * @param {string} options.next_tag_open The next page open tag. If you set to display next page, this will be placed before link to the next page. Default is 1 space.
      * 
      * @param {number} options.number_adjacent_pages The number of adjacent pages before and after the current page. Defaut is 5.
-     * @param {boolean} options.number_display Display the page numbers or not. Set to true to display, false not to display. Default is `true`.
+     * @param {boolean} options.number_display Display the page numbers or not. Set to `true` to display, `false` not to display. Default is `true`.
      * @param {object} options.number_page_link_attributes The number page link attributes in object where key is attribute name. 
      *              Example `{'class' => 'my class'}`. Must not contains `href`, `data-rd-pagination` attributes.
-     * @param {string} options.number_tag_close The page number tag close. If you set to display page number, this will be placed after link to the page number. Default is 1 space.
-     * @param {string} options.number_tag_open The page number tag open. If you set to display page number, this will be placed before link to the page number. Default is 1 space.
+     * @param {string} options.number_tag_close The page number close tag. If you set to display page number, this will be placed after link to the page number. Default is 1 space.
+     * @param {string} options.number_tag_open The page number open tag. If you set to display page number, this will be placed before link to the page number. Default is 1 space.
      * 
-     * @param {string} options.overall_tag_close The overall tag close. It will be place at the very end of displaying page numbers. Default is empty.
-     * @param {string} options.overall_tag_open The overall tag open. It will be place at the very beginning of displaying page numbers. Default is empty.
+     * @param {string} options.overall_tag_close The overall close tag. It will be place at the very end of displaying page numbers. Default is empty.
+     * @param {string} options.overall_tag_open The overall open tag. It will be place at the very beginning of displaying page numbers. Default is empty.
      * 
-     * @param {boolean} options.previous_page_always_show If you are at first page the previous page link will not show if you set this value to false, if you set to true it will be always show the previous page link. Default is `false`.
+     * @param {boolean} options.previous_page_always_show If you are at first page the previous page link will not show if you set this value to `false`, if you set to `true` it will be always show the previous page link. Default is `false`.
      * @param {object} options.previous_page_link_attributes The previous page link attributes in object where key is attribute name. 
      *              Example `{'class' => 'my class'}`. Must not contains `href`, `data-rd-pagination` attributes.
-     * @param {string|false} options.previous_page_text The link text of the paginate that will go to the previous page. Set to false to not displaying previous page link.
-     * @param {string} options.previous_tag_close The previous page tag close. If you set to display previous page, this will be placed after link to the previous page. Default is 1 space.
-     * @param {string} options.previous_tag_open The previous page tag open. If you set to display previous page, this will be placed before link to the previous page. Default is 1 space.
+     * @param {string|false} options.previous_page_text The link text of the paginate that will go to the previous page. Set to `false` to not displaying previous page link at all.
+     * @param {string} options.previous_tag_close The previous page close tag. If you set to display previous page, this will be placed after link to the previous page. Default is 1 space.
+     * @param {string} options.previous_tag_open The previous page open tag. If you set to display previous page, this will be placed before link to the previous page. Default is 1 space.
      * 
-     * @param {boolean} options.unavailable_display Display unavailable page (...) or not. Set to true to display, false to not display. Default is `false`.
+     * @param {boolean} options.unavailable_display Display unavailable page (...) or not. Set to `true` to display, `false` to not display. Default is `false`.
      * @param {string} options.unavailable_text The unavailable page text. Basically it is something to show that there are pages between these range such as 3 dots text. (...) Default is `&hellip;`.
-     * @param {string} options.unavailable_tag_close The unavailable page tag close. If you set to display unavailable page, this will be placed after unavailable page (...). Default is 1 space.
-     * @param {string} options.unavailable_tag_open The unavailable page tag open. If you set to display unavailable page, this will be placed before unavailable page (...). Default is 1 space.
-     * @param {number|false} options.unavailable_after Number of pages to display after last unavailable page. Set number as integer or set to false to not display the pages after unavailable. Default is 2.
-     * @param {number|false} options.unavailable_before Number of pages to display before first unavailable page. Set number as integer or set to false to not display the pages before unavailable. Default is 2.
+     * @param {string} options.unavailable_tag_close The unavailable page close tag. If you set to display unavailable page, this will be placed after unavailable page (...). Default is 1 space.
+     * @param {string} options.unavailable_tag_open The unavailable page open tag. If you set to display unavailable page, this will be placed before unavailable page (...). Default is 1 space.
+     * @param {number|false} options.unavailable_after Number of pages to display after last unavailable page. Set number as integer or set to `false` to not display the pages after unavailable. Default is 2.
+     * @param {number|false} options.unavailable_before Number of pages to display before first unavailable page. Set number as integer or set to `false` to not display the pages before unavailable. Default is 2.
      * 
      * @param {object} options.events The events to be called.
      * @param {function} options.events.onclick On click event. This event is fired after displayed pagination. Set callback function here to call when user clicked. If you set callback function, this class will not use `preventDefault()` to let you handle it.
+     * 
+     * @throws Throw the errors if wrong argument type.
      */
     constructor(selector, options = {}) {
         if (typeof(selector) !== 'string') {
@@ -577,13 +580,8 @@ class RdPagination {
             return null;
         }
 
-        // render the pagination. --------------------------------------
-        const pagination_rendered = this.#getPaginationHTML(pagination_data);
-        // end render the pagination. ----------------------------------
-
-        document.querySelectorAll(this.#selector)?.forEach((eachDOM) => {
-            eachDOM.innerHTML = pagination_rendered;
-        });
+        // use `reCreateLinks()` to reduce call to the same code twice or more.
+        this.reCreateLinks(pagination_data);
     }// createLinks
 
 
@@ -837,10 +835,11 @@ class RdPagination {
 
     /**
      * Re-create links on all selector matched.  
-     * This is for reduce process on method `getPaginationData()` in case there is already one.
+     * This is for reduce process on method `getPaginationData()` inside `createLinks()` in case there is already one.
      * 
      * @since 0.0.4
      * @param {object} pagination_data Processed data from `getPaginationData()`.
+     * @throws Throw the errors if wrong argument type.
      */
     reCreateLinks(pagination_data) {
         if (typeof(pagination_data) !== 'object') {
